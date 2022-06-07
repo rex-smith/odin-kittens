e03cc9919b2ad2b77fc0b564d4b17c0cdb98d597
rex-smith <rex.smith17@gmail.com>
2022-06-07 15:28:22 -0400

    Added kittens routes and controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,58 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    respond_to do |format|
+      if @kitten.save
+        format.html { redirect_to kitten_url(@kitten), notice: "Kitten was successfully created." }
+        format.json { render :show, status: :created, location: @kitten }
+      else
+        format.html { render: :new, status: :unprocessable_entity }
+        format.json { render json: @kitten.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    # @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @kitten.update(kitten_params)
+        format.html { redirect_to kitten_url(@kitten), notice: "Kitten was successfully updated." }
+        format.json { render :show, status: :ok, location: @kitten }
+      else
+        format.html { render: :edit, status: :unprocessable_entity }
+        format.json { render json: @kitten.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @kitten.destroy
+
+    respond_to do |format|
+      format.html { redirect_to kittens_url, notice: 'Kitten successfully eliminated.'}
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end

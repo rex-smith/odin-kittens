494f896c57900581a2b566f760241740d699b082
rex-smith <rex.smith17@gmail.com>
2022-06-07 23:14:59 -0400

    Added ability to respond to json requests

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,11 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kittens }
+    end
   end
 
   def new
@@ -9,19 +14,22 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kitten }
+    end
   end
 
   def create
     @kitten = Kitten.new(kitten_params)
 
-    respond_to do |format|
-      if @kitten.save
-        format.html { redirect_to root_path, notice: "Kitten was successfully created." }
-        format.json { render :show, status: :created, location: @kitten }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @kitten.errors, status: :unprocessable_entity }
-      end
+    if @kitten.save
+      flash[:success] = "Kitten was successfully created."
+      redirect_to root_path
+    else
+      flash[:errpr] = "Something went wrong"
+      render :new
     end
   end
 
@@ -32,24 +40,23 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find(params[:id])
 
-    respond_to do |format|
-      if @kitten.update(kitten_params)
-        format.html { redirect_to root_path, notice: "Kitten was successfully updated." }
-        format.json { render :show, status: :ok, location: @kitten }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @kitten.errors, status: :unprocessable_entity }
-      end
+    if @kitten.update(kitten_params)
+      flash[:success] = "Kitten was successfully updated."
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render :edit
     end
   end
   
   def destroy
     @kitten = Kitten.find(params[:id])
-    @kitten.destroy
-
-    respond_to do |format|
-      format.html { redirect_to kittens_url, notice: 'Kitten successfully eliminated.'}
-      format.json { head :no_content }
+    if @kitten.destroy
+      flash[:success] = 'Kitten successfully eliminated.'
+      redirect_to kittens_url 
+    else 
+      flash[:error] = 'Something went wrong'
+      redirect_to kittens_url 
     end
   end
 
